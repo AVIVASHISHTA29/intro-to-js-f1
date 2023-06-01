7c65e45ca4df29d093e4969ce0d03c440c8ea397
avivashishta29 <avivashishta@gmail.com>
2023-06-01 21:36:48 +0530

    first

---
+++ script.js
@@ -0,0 +1,45 @@
+var myName = "Avi";
+console.log("Hello World");
+var newString = "My Name is - " + myName;
+var myNumber = 10 + 20;
+myNumber = myNumber + 20;
+var flag = true; //false
+console.log(newString);
+console.log(myNumber);
+
+// Single Line comment -
+
+/*
+Hi 
+This 
+Is MY Multi line
+Comment
+*/
+
+// In Built Fncs?
+// Alerts -
+// myFnc()
+
+// alert("Today is your last class for F1 :)");
+// Prompt - if the user clicks OK -> the value will be stored in the variable - string
+// If the user clicks cancel -> the variable will be null
+
+// var ans = prompt("Please Enter your name");
+// if (ans != null) {
+//   alert("Answer to the prompt - " + ans);
+// } else {
+//   alert("Got Null");
+// }
+
+// Confirm gives a boolean
+// var ans = confirm("This website trakcs your data, click Yes to proceed");
+// if (ans) {
+//   alert("You clicked on okay");
+// } else {
+//   alert("You clicked on cancel");
+// }
+
+var myName = "Avi";
+var job = "instructor";
+console.log("Hi" + myName);
+console.log("Hi", " ", myName, job, 5, true);
